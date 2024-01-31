30df353237d815683104abf97b2773c5bc8fbaca
InternatBlackhole <timmy@timkr.si>
2024-01-31 18:17:58 +0100

    client done. not tested

--- client/client.go
+++ client/client.go
@@ -2,113 +2,115 @@ package client
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"math/rand"
-	"strconv"
 	"time"
 	pb "tkNaloga04/rpc"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+var (
+	head pb.PutProviderClient
+	read pb.ReadProviderClient
 )
 
 func main() {
-
-	hName := flag.String("s", "", "Hostname of replication provider")
-
-	flag.Parse()
-
-	hostname := *hName
-
-	if hostname == "" {
-		fmt.Println("Hostname not provided")
-		return
-	}
-
-	fmt.Println("Client waiting 200 ms...")
-	time.Sleep(200 * time.Millisecond)
-
-	fmt.Println("Client starting...")
-
-	fmt.Printf("Connecting to head node on port %d and tail node in port %d\n", startPort, startPort+totalNodes-1)
-
-	head, tail := getReplicatorNode(startPort), getReplicatorNode(startPort+totalNodes-1)
-
-	fmt.Println("Connected to head and tail node")
-	fmt.Println("Sending put request to head node")
-
-	ncon := context.Background()
-	const toGen = 20
-
-	for i := 0; i < toGen; i++ {
-		//generate 10 key value pairs
-		_, err := head.Put(ncon, &pb.Entry{Key: "key" + strconv.Itoa(i), Value: "value" + strconv.Itoa(i)})
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Put key: %s, value: %s\n", "key"+strconv.Itoa(i), "value"+strconv.Itoa(i))
-	}
-
-	fmt.Println()
-	fmt.Println("Put requests sent to head node")
-	fmt.Println("Sending get request to tail node")
-
-	for i := 0; i < toGen/2; i++ {
-		ent, err := tail.Get(ncon, &pb.Entry{Key: "key" + strconv.Itoa(rand.Intn(toGen/2))})
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Printf("Got key: %s, value: %s\n", ent.Key, ent.Value)
-		}
-	}
-
-	fmt.Println()
-	fmt.Println("Sending modify put request to head node")
-
-	for i := 0; i < toGen/2; i++ {
-		key, val := "key"+strconv.Itoa(rand.Intn(toGen)), "value"+strconv.Itoa(rand.Intn(toGen))
-		_, err := head.Put(ncon, &pb.Entry{Key: key, Value: val})
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Modified key: %s, to value: %s\n", key, val)
-	}
-
-	fmt.Println()
-	fmt.Println("Sending get request to tail node")
-	for i := 0; i < toGen/2; i++ {
-		ent, err := tail.Get(ncon, &pb.Entry{Key: "key" + strconv.Itoa(rand.Intn(toGen/2))})
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Printf("Got key: %s, value: %s\n", ent.Key, ent.Value)
+	printUsage()
+	//ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	var err error
+	for {
+		var cmd string
+		fmt.Scanln(&cmd)
+		switch cmd {
+		case "put":
+			if head == nil {
+				fmt.Println("Not connected to head node. Connect to a head node first.")
+				continue
+			}
+			var key, value string
+			fmt.Scanln(&key, &value)
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+			_, err := head.Put(ctx, &pb.Entry{Key: key, Value: value})
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+		case "get":
+			if read == nil {
+				fmt.Println("Not connected to read node. Connect to a read node first.")
+				continue
+			}
+			var key string
+			fmt.Scanln(&key)
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+			ent, err := read.Get(ctx, &wrapperspb.StringValue{Value: key})
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+			fmt.Println("Value: ", ent.Value)
+		case "quit":
+			return
+		case "ch":
+			if head != nil {
+				fmt.Println("Already connected to head node. Disconnecting.")
+			}
+			var hostname string
+			fmt.Scanln(&hostname)
+			head, err = getPutProvider(hostname)
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+			fmt.Println("Connected to head node: ", hostname)
+		case "cn":
+			if read != nil {
+				fmt.Println("Already connected to read node. Disconnecting.")
+			}
+			var hostname string
+			fmt.Scanln(&hostname)
+			read, err = getReadProvider(hostname)
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+			fmt.Println("Connected to read node: ", hostname)
+		default:
+			fmt.Println("Unknown command")
 		}
 	}
-
-	fmt.Println("Client done")
 }
 
-func getPutProvider(hostname string) pb.PutProviderClient {
-	conn, err := grpc.Dial(hostname, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		panic(err)
-	}
-	return pb.NewPutProviderClient(conn)
+func printUsage() {
+	fmt.Print(
+		"Usage:\n\tch <hostname:port> - connect to head node\n\tcn <hostname:port> - connect to node for reading" +
+			"\n\tput <key> <value>\n\tget <key>\n\tquit - exit the program")
 }
 
-func getReadProvider(hostname string) pb.ReadProviderClient {
+func getPutProvider(hostname string) (pb.PutProviderClient, error) {
 	conn, err := grpc.Dial(hostname, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return pb.NewReadProviderClient(conn)
+	return pb.NewPutProviderClient(conn), nil
 }
 
-func getReplicatorNode(hostname string) pb.ReplicationProviderClient {
+func getReadProvider(hostname string) (pb.ReadProviderClient, error) {
 	conn, err := grpc.Dial(hostname, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return pb.NewReplicationProviderClient(conn)
+	return pb.NewReadProviderClient(conn), nil
+}
+
+func getControllerNode(hostname string) (pb.ControllerClient, error) {
+	conn, err := grpc.Dial(hostname, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return pb.NewControllerClient(conn), nil
 }
